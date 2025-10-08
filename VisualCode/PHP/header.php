6bc7f3b6935df0b0c1a661e066f958a9c9d286a4
Carlos Henrique <rikcar22@gmail.com>
2025-10-08 14:10:12 -0300

    Commit Trocando as fotos para mais qualidade e mudanças no php

--- VisualCode/PHP/header.php
+++ VisualCode/PHP/header.php
@@ -2,18 +2,17 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <link rel="icon" href="Serenity.jpeg" type="image/png">
+    <link rel="icon" href="../IMAGENS/Serenity.jpeg" type="image/png">
     <title>SPA Serenity Login</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
 <header>
     <div class="logo">
-        <img src="Serenity.jpeg" alt="SPA Serenity" class="logo-img">
+        <img src="../IMAGENS/Serenity.jpeg" alt="SPA Serenity" class="logo-img">
         SPA Serenity
     </div>
     <nav>
-        <a href="index.php" class="<?= basename($_SERVER['PHP_SELF'])=='index.php'?'active':'' ?>">Login</a>
         <?php if(isset($_SESSION['nome'])): ?>
             <a href="agendar.php" class="<?= basename($_SERVER['PHP_SELF'])=='agendar.php'?'active':'' ?>">Agendar</a>
             <a href="logout.php">Sair</a>
